ba073adad9c5f1817cfd9c0035f7ddce22572776
iitsoftware <am@iit.de>
2022-12-21 16:05:17 +0100

    JMSConsumerImpl implemented

--- src/main/java/com/swiftmq/jms/v750/JMSConsumerImpl.java
+++ src/main/java/com/swiftmq/jms/v750/JMSConsumerImpl.java
@@ -1,58 +1,104 @@
 package com.swiftmq.jms.v750;
 
-import javax.jms.JMSConsumer;
-import javax.jms.JMSRuntimeException;
-import javax.jms.Message;
-import javax.jms.MessageListener;
+import javax.jms.*;
 
 public class JMSConsumerImpl implements JMSConsumer {
+    MessageConsumer consumer;
+
+    public JMSConsumerImpl(MessageConsumer consumer) {
+        this.consumer = consumer;
+    }
+
     @Override
     public String getMessageSelector() {
-        return null;
+        try {
+            return consumer.getMessageSelector();
+        } catch (JMSException e) {
+            throw new JMSRuntimeException(e.getMessage());
+        }
     }
 
     @Override
     public MessageListener getMessageListener() throws JMSRuntimeException {
-        return null;
+        try {
+            return consumer.getMessageListener();
+        } catch (JMSException e) {
+            throw new JMSRuntimeException(e.getMessage());
+        }
     }
 
     @Override
     public void setMessageListener(MessageListener messageListener) throws JMSRuntimeException {
-
+        try {
+            consumer.setMessageListener(messageListener);
+        } catch (JMSException e) {
+            throw new JMSRuntimeException(e.getMessage());
+        }
     }
 
     @Override
     public Message receive() {
-        return null;
+        try {
+            return consumer.receive();
+        } catch (JMSException e) {
+            throw new JMSRuntimeException(e.getMessage());
+        }
     }
 
     @Override
-    public Message receive(long l) {
-        return null;
+    public Message receive(long timeout) {
+        try {
+            return consumer.receive(timeout);
+        } catch (JMSException e) {
+            throw new JMSRuntimeException(e.getMessage());
+        }
     }
 
     @Override
     public Message receiveNoWait() {
-        return null;
+        try {
+            return consumer.receiveNoWait();
+        } catch (JMSException e) {
+            throw new JMSRuntimeException(e.getMessage());
+        }
     }
 
     @Override
     public <T> T receiveBody(Class<T> aClass) {
-        return null;
+        Message msg = receive();
+        try {
+            return msg == null ? null : msg.getBody(aClass);
+        } catch (JMSException e) {
+            throw new JMSRuntimeException(e.getMessage());
+        }
     }
 
     @Override
-    public <T> T receiveBody(Class<T> aClass, long l) {
-        return null;
+    public <T> T receiveBody(Class<T> aClass, long timeout) {
+        Message msg = receive(timeout);
+        try {
+            return msg == null ? null : msg.getBody(aClass);
+        } catch (JMSException e) {
+            throw new JMSRuntimeException(e.getMessage());
+        }
     }
 
     @Override
     public <T> T receiveBodyNoWait(Class<T> aClass) {
-        return null;
+        Message msg = receiveNoWait();
+        try {
+            return msg == null ? null : msg.getBody(aClass);
+        } catch (JMSException e) {
+            throw new JMSRuntimeException(e.getMessage());
+        }
     }
 
     @Override
     public void close() {
-
+        try {
+            consumer.close();
+        } catch (JMSException e) {
+            throw new JMSRuntimeException(e.getMessage());
+        }
     }
 }
