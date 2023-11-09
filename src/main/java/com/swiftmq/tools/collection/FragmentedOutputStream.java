93413d48e4953e70708167a7f1506450ff08a49e
iitsoftware <am@iit.de>
2023-11-09 10:51:31 +0100

    SMQPOutputHandler using Fragments and removed ThreadLocals

--- src/main/java/com/swiftmq/tools/collection/FragmentedOutputStream.java
+++ src/main/java/com/swiftmq/tools/collection/FragmentedOutputStream.java
@@ -17,13 +17,27 @@
 
 package com.swiftmq.tools.collection;
 
-import java.io.IOException;
+import com.swiftmq.tools.gc.WeakPool;
+
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The FragmentedOutputStream class is designed for efficient network transmission
+ * by breaking down data into manageable fragments. It provides an output stream that
+ * collects written data into fixed-size byte arrays, referred to as fragments.
+ * This approach is particularly useful for network protocols that require data to be
+ * sent in discrete chunks and can help to align with network packet sizes to optimize transmission.
+ * <p>
+ * The class offers functionality to write data byte by byte or in byte arrays, manage the size of each fragment,
+ * and to optionally include the overall length of the data at the beginning of the stream.
+ * It leverages an internal pooling mechanism to minimize garbage collection overhead by reusing byte arrays
+ * where possible. This is particularly useful in high-throughput scenarios where stream reset and reuse are common.
+ */
 public class FragmentedOutputStream {
     private final List<Fragment> fragments = new ArrayList<>();
+    private final WeakPool<Fragment> fragmentPool = new WeakPool<>();
     private final int fragmentSize;
     private boolean includeLength;
     private int totalLength = 0;
@@ -35,26 +49,36 @@ public class FragmentedOutputStream {
         this.fragmentSize = fragmentSize;
         this.includeLength = includeLength;
         // Initialize the first fragment, possibly leaving space for the length prefix
-        fragments.add(new Fragment(new byte[fragmentSize], includeLength ? 4 : 0));
-        totalLength = includeLength ? 4 : 0;
+        firstFragment();
+        totalLength = 0; // without the length field
+    }
+
+    private void firstFragment() {
+        Fragment fragment = fragmentPool.get(() -> new Fragment(new byte[fragmentSize], 0));
+        fragment.length = includeLength ? 4 : 0;
+        fragments.add(fragment);
     }
 
     private void ensureCapacity(int length) {
         if (fragments.isEmpty() || fragments.get(fragments.size() - 1).isFull()) {
-            fragments.add(new Fragment(new byte[fragmentSize], 0));
+            fragments.add(fragmentPool.get(() -> new Fragment(new byte[fragmentSize], 0)));
         }
     }
 
-    public void write(int b) throws IOException {
+    public Fragment getCurrentFragment() {
+        return fragments.get(fragments.size() - 1);
+    }
+
+    public void write(int b) {
         ensureCapacity(1);
-        Fragment currentFragment = fragments.get(fragments.size() - 1);
+        Fragment currentFragment = getCurrentFragment();
         currentFragment.data[currentFragment.length++] = (byte) b;
         if (includeLength) {
             totalLength++;
         }
     }
 
-    public void write(byte[] b, int off, int len) throws IOException {
+    public void write(byte[] b, int off, int len) {
         if (b == null) {
             throw new NullPointerException();
         } else if ((off < 0) || (off > b.length) || (len < 0) ||
@@ -67,7 +91,7 @@ public class FragmentedOutputStream {
         int bytesWritten = 0;
         while (bytesWritten < len) {
             ensureCapacity(len - bytesWritten);
-            Fragment currentFragment = fragments.get(fragments.size() - 1);
+            Fragment currentFragment = getCurrentFragment();
             int bytesToWrite = Math.min(len - bytesWritten, fragmentSize - currentFragment.length);
             System.arraycopy(b, off + bytesWritten, currentFragment.data, currentFragment.length, bytesToWrite);
             bytesWritten += bytesToWrite;
@@ -86,12 +110,16 @@ public class FragmentedOutputStream {
     }
 
     public void reset() {
-        // Clear the existing fragments
+        // Clear the existing fragments and checkin to the pool
+        fragments.forEach(f -> {
+            f.length = 0;
+            fragmentPool.checkIn(f);
+        });
         fragments.clear();
         // Add a new Fragment, with space for length if required
-        fragments.add(new Fragment(new byte[fragmentSize], includeLength ? 4 : 0));
-        // Reset total size, including the space for the length prefix if necessary
-        totalLength = includeLength ? 4 : 0;
+        firstFragment();
+        // Reset total size
+        totalLength = 0;
     }
 
     public int getFragmentCount() {
@@ -112,6 +140,14 @@ public class FragmentedOutputStream {
         public Fragment(byte[] data, int length) {
             this.data = data;
             this.length = length;
+        }
+
+        public byte[] getData() {
+            return data;
+        }
+
+        public int getLength() {
+            return length;
         }
 
         public boolean isFull() {
