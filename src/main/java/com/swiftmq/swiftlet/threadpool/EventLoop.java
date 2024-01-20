dde2e788f87a64b3f06dc76b6283beb28f5d9601
iitsoftware <am@iit.de>
2024-01-20 19:12:01 +0100

    removed executeInNewThread from EventLoop

--- src/main/java/com/swiftmq/swiftlet/threadpool/EventLoop.java
+++ src/main/java/com/swiftmq/swiftlet/threadpool/EventLoop.java
@@ -1,11 +1,7 @@
 package com.swiftmq.swiftlet.threadpool;
 
-import java.util.concurrent.CompletableFuture;
-
 public interface EventLoop {
     void submit(Object event);
-
-    CompletableFuture<?> executeInNewThread(Runnable task);
 
     void close();
 }
